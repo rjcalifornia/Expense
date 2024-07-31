e4bdf266d57e8b35fac2a41bfd67103c78f3e9c9
rjcalifornia <rodolfohernan20@gmail.com>
2024-07-31 11:17:34 -0600

    Changes to be committed: 	modified:   lib/ui/screens/home.dart

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -10,8 +10,75 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Test"),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 3,
+                  height: MediaQuery.of(context).size.height / 4,
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40.0),
+                              color: Color(0xC92758ED)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Expense",
+                                style: TextStyle(
+                                    color: Color(0xffE6EAFF),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14),
+                              ),
+                              Text(
+                                "Welcome back",
+                                style: TextStyle(
+                                    color: Color(0xffDADADC),
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Center(
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width / 3,
+                                  padding: EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(40.0),
+                                      color: Color(0xffDADADC)),
+                                  child: Column(
+                                    children: [],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const Text("Column 1"),
+              ],
+            ),
+          ),
+          Expanded(
+              child: Column(
+            children: [Text("Column 2")],
+          ))
+        ],
+      ),
     );
   }
 }
